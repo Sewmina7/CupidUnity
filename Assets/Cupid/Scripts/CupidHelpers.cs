5e9265c629285f9148e081622cbacb4a7abd6df2
Sewmina Dilshan <sewmina7@gmail.com>
2022-11-10 02:08:13 +0530

    Init

---
+++ Assets/Cupid/Scripts/CupidHelpers.cs
@@ -0,0 +1,66 @@
+using System;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public static class Cupid
+{
+    private static string serverAddress;
+    public static string ServerAddress => serverAddress;
+    public static string CupidURI => "http://" + serverAddress + ":" + port;
+
+    private static int port;
+    public static int Port => port;
+
+    private static string password;
+    private static CupidSettings settings = null;
+    public static CupidSettings Settings => settings;
+
+    public static bool isInitialized => settings!=null;
+
+
+    public static async Task Init(string _serverAddress, int _port, string _password){
+        serverAddress= _serverAddress;
+        port = _port;
+        password = _password;
+
+        using (UnityWebRequest www = UnityWebRequest.Get(CupidURI + "/settings?password="+password))
+        {
+            var operation = www.SendWebRequest();
+            while (!operation.isDone)
+            {
+                await Task.Yield();
+            }
+
+            try{
+                settings = JsonUtility.FromJson<CupidSettings>(www.downloadHandler.text);
+                if(settings==null){throw new NullReferenceException();}
+                Debug.Log("Cupid init success");
+            }catch(Exception e){
+                Debug.Log("Error retreiving settings from server " + e.Message);
+                Debug.Log(www.downloadHandler.text);
+            }
+        }
+    }
+
+    
+    public static string RandomUsername{
+        get{
+            string pool="ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            string output ="";
+            for(int i=0; i < 5; i++){
+                output += pool.ToCharArray()[UnityEngine.Random.Range(0,pool.Length)];
+            }
+
+            return output;
+        }
+    }
+}
+
+[System.Serializable]
+public class CupidSettings
+{
+    public int minimum_players;
+    public int maximum_players;
+    public int waiting_time;
+}
---
+++ Assets/Cupid/Scripts/CupidLobby.cs
@@ -0,0 +1,85 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CupidLobby : MonoBehaviour
+{
+    public GameObject MatchmakingUI;
+    public Scene GameScene;
+    [Header("Server info")]
+    public string serverAddress = "xx.xx.xxx.xx";
+    public int cupidPort = 1601;
+    public string password = "xyz@123";
+
+    public static string Username
+    {
+        get
+        {
+            if (PlayerPrefs.HasKey("username"))
+            {
+                return PlayerPrefs.GetString("username");
+            }else{
+                string username = Cupid.RandomUsername;
+                PlayerPrefs.SetString("username",username);
+                PlayerPrefs.Save();
+
+                return username;
+            }
+        }
+    }
+
+    void Start()
+    {
+        Cupid.Init(serverAddress, cupidPort, password);
+    }
+    bool matchmaking = false;
+    public void Matchmake()
+    {
+        StartCoroutine(matchmake());
+    }
+
+    IEnumerator matchmake()
+    {
+        matchmaking = true;
+        while (matchmaking)
+        {
+            RefreshMatchmakingPanel();
+
+            WWW req = new WWW(Cupid.CupidURI + "/?password=" + password + "&&username=" + Username);
+            yield return req;
+            Debug.Log(req.text);
+            string[] data = req.text.Split(',');
+            if(data.Length ==2){
+                if(data[0] == "1"){
+                    //Game started
+                    SceneManager.LoadScene(GameScene.name);
+                    matchmaking=false;
+                    break;
+                }else{
+                    //Game not started gotta continue
+                }
+                int gamePort = -1;
+                try{
+                    gamePort = int.Parse(data[1]);
+                }catch(Exception e){
+                    Debug.LogError("Couldn't parse game port: " + req.text);
+                }
+            }
+
+            yield return new WaitForSeconds(1);
+        }
+    }
+
+    public void Cancel()
+    {
+        matchmaking = false;
+        RefreshMatchmakingPanel();
+    }
+
+    void RefreshMatchmakingPanel()
+    {
+        MatchmakingUI.SetActive(matchmaking);
+    }
+}
